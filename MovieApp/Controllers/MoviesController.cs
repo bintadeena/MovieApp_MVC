4d3eee0c8b0e2982098ee606506725d3fdc6dac2
Binta Deena <binta.deena@hotmail.com>
2020-04-13 11:02:07 +0530

    First Commit- Project Commit

---
+++ MovieApp/Controllers/MoviesController.cs
@@ -0,0 +1,22 @@
+﻿using MovieApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieApp.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            // return View(movie);
+            //return Content("Hello World from Content Result");
+            //return HttpNotFound();
+            return RedirectToAction("Index", "Home", new { page = 1, sortby = "name" });
+        }
+    }
+}
