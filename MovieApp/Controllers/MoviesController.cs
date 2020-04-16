65b00788c83f1d65cba2451b4a0e3c88bd8a2f02
Binta Deena <binta.deena@hotmail.com>
2020-04-16 12:55:15 +0530

    commit 2- viewdata, viewbag, tempdata

--- MovieApp/Controllers/MoviesController.cs
+++ MovieApp/Controllers/MoviesController.cs
@@ -1,4 +1,6 @@
-﻿using MovieApp.Models;
+﻿using Microsoft.Ajax.Utilities;
+using MovieApp.Models;
+using MovieApp.Viewmodels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,10 +15,77 @@ namespace MovieApp.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
-            // return View(movie);
+            var customers = new List<Customer> {
+                new Customer {Name="customer1"},
+                new Customer{Name="customer2"}
+            };
+            var viewmodel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+            //ViewData["Movie"] = movie;
+            // ViewBag.Movie = movie;
+            return View(viewmodel);
+
             //return Content("Hello World from Content Result");
             //return HttpNotFound();
-            return RedirectToAction("Index", "Home", new { page = 1, sortby = "name" });
+            // return RedirectToAction("Index", "Home", new { page = 1, sortby = "name" });
         }
+        public ActionResult Edit(int movieid)
+        {
+            return Content("Id= " + movieid);
+        }
+        public ActionResult Index(int pageindex, string sortBy)
+        {
+            //if (!pageindex.HasValue)
+            //    pageindex = 1;
+            if (string.IsNullOrEmpty(sortBy))
+                sortBy = "name";
+            return Content(String.Format("Pageindex={0},Sort By={1}", pageindex, sortBy));
+        }
+        [Route("movies/released/{year}/{month:range(1,12)}")]
+        public ActionResult ByReleasedDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+        //creating login page
+        public ActionResult Login()
+        {
+            ViewBag.Countries = new List<string>()
+            {
+                "India","US","Australia", "Finland","Sweden"
+            };
+            //ViewData["Countries"] = new List<string>()
+            //{
+            //    "India","US","Australia", "Finland","Sweden"
+            //};
+            
+            return View();
+           
+
+        }
+        public ActionResult HomePage()
+        {
+            string DisplayName = TempData["DisplayName"].ToString();
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(RandomMovieViewModel viewmodel)
+        {
+
+            var username = viewmodel.Uname;
+            var password = viewmodel.Password;
+            
+            if (username == "binta" && password == "selu123")
+            {
+                TempData["DisplayName"] = username;
+                return RedirectToAction("HomePage");
+            }
+          
+            return View();
+        }
+
     }
 }
---
+++ MovieApp/Viewmodels/RandomMovieViewModel.cs
@@ -0,0 +1,20 @@
+﻿using MovieApp.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MovieApp.Viewmodels
+{
+    public class RandomMovieViewModel
+    {
+        public Movie Movie { get; set; }
+        public List<Customer> Customers { get; set; }
+        [Required]
+        public string Uname { get; set; }
+        [Required]
+        public string Password { get; set; }
+
+    }
+}
